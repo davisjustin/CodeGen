558cb3d719ef11e0ee1e85674a267b9add9ee7b9
Davis Justin <davisjustin999@hotmail.com>
2015-09-11 13:10:50 +0530

    CodeGen alpha upload

---
+++ CodeGen/CodeGen/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace CodeGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbQ = new System.Windows.Forms.TextBox();
+            this.tbO1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbO2 = new System.Windows.Forms.TextBox();
+            this.tbO3 = new System.Windows.Forms.TextBox();
+            this.tbO4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rO1 = new System.Windows.Forms.RadioButton();
+            this.rO2 = new System.Windows.Forms.RadioButton();
+            this.rO3 = new System.Windows.Forms.RadioButton();
+            this.rO4 = new System.Windows.Forms.RadioButton();
+            this.bGen = new System.Windows.Forms.Button();
+            this.bNxt = new System.Windows.Forms.Button();
+            this.bPrev = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lindex = new System.Windows.Forms.Label();
+            this.cbIg = new System.Windows.Forms.CheckBox();
+            this.cbAnimDelay = new System.Windows.Forms.CheckBox();
+            this.cbEnbCol = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbCorr = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbWrng = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.bDefCol = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Question:";
+            // 
+            // tbQ
+            // 
+            this.tbQ.Location = new System.Drawing.Point(68, 95);
+            this.tbQ.Name = "tbQ";
+            this.tbQ.Size = new System.Drawing.Size(682, 20);
+            this.tbQ.TabIndex = 1;
+            // 
+            // tbO1
+            // 
+            this.tbO1.Location = new System.Drawing.Point(68, 153);
+            this.tbO1.Name = "tbO1";
+            this.tbO1.Size = new System.Drawing.Size(100, 20);
+            this.tbO1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Option 1:";
+            // 
+            // tbO2
+            // 
+            this.tbO2.Location = new System.Drawing.Point(68, 179);
+            this.tbO2.Name = "tbO2";
+            this.tbO2.Size = new System.Drawing.Size(100, 20);
+            this.tbO2.TabIndex = 4;
+            // 
+            // tbO3
+            // 
+            this.tbO3.Location = new System.Drawing.Point(68, 205);
+            this.tbO3.Name = "tbO3";
+            this.tbO3.Size = new System.Drawing.Size(100, 20);
+            this.tbO3.TabIndex = 5;
+            // 
+            // tbO4
+            // 
+            this.tbO4.Location = new System.Drawing.Point(68, 231);
+            this.tbO4.Name = "tbO4";
+            this.tbO4.Size = new System.Drawing.Size(100, 20);
+            this.tbO4.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Option 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Option 3:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Option 4:";
+            // 
+            // rO1
+            // 
+            this.rO1.AutoSize = true;
+            this.rO1.Location = new System.Drawing.Point(174, 156);
+            this.rO1.Name = "rO1";
+            this.rO1.Size = new System.Drawing.Size(14, 13);
+            this.rO1.TabIndex = 10;
+            this.rO1.TabStop = true;
+            this.rO1.UseVisualStyleBackColor = true;
+            // 
+            // rO2
+            // 
+            this.rO2.AutoSize = true;
+            this.rO2.Location = new System.Drawing.Point(174, 182);
+            this.rO2.Name = "rO2";
+            this.rO2.Size = new System.Drawing.Size(14, 13);
+            this.rO2.TabIndex = 11;
+            this.rO2.TabStop = true;
+            this.rO2.UseVisualStyleBackColor = true;
+            // 
+            // rO3
+            // 
+            this.rO3.AutoSize = true;
+            this.rO3.Location = new System.Drawing.Point(174, 208);
+            this.rO3.Name = "rO3";
+            this.rO3.Size = new System.Drawing.Size(14, 13);
+            this.rO3.TabIndex = 12;
+            this.rO3.TabStop = true;
+            this.rO3.UseVisualStyleBackColor = true;
+            // 
+            // rO4
+            // 
+            this.rO4.AutoSize = true;
+            this.rO4.Location = new System.Drawing.Point(174, 234);
+            this.rO4.Name = "rO4";
+            this.rO4.Size = new System.Drawing.Size(14, 13);
+            this.rO4.TabIndex = 13;
+            this.rO4.TabStop = true;
+            this.rO4.UseVisualStyleBackColor = true;
+            // 
+            // bGen
+            // 
+            this.bGen.Location = new System.Drawing.Point(499, 385);
+            this.bGen.Name = "bGen";
+            this.bGen.Size = new System.Drawing.Size(75, 23);
+            this.bGen.TabIndex = 16;
+            this.bGen.Text = "Generate";
+            this.bGen.UseVisualStyleBackColor = true;
+            this.bGen.Click += new System.EventHandler(this.bGen_Click);
+            // 
+            // bNxt
+            // 
+            this.bNxt.Location = new System.Drawing.Point(418, 385);
+            this.bNxt.Name = "bNxt";
+            this.bNxt.Size = new System.Drawing.Size(75, 23);
+            this.bNxt.TabIndex = 15;
+            this.bNxt.Text = "Next >";
+            this.bNxt.UseVisualStyleBackColor = true;
+            this.bNxt.Click += new System.EventHandler(this.bNxt_Click);
+            // 
+            // bPrev
+            // 
+            this.bPrev.Location = new System.Drawing.Point(337, 385);
+            this.bPrev.Name = "bPrev";
+            this.bPrev.Size = new System.Drawing.Size(75, 23);
+            this.bPrev.TabIndex = 14;
+            this.bPrev.Text = "< Prev";
+            this.bPrev.UseVisualStyleBackColor = true;
+            this.bPrev.Click += new System.EventHandler(this.bPrev_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(65, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Question:";
+            // 
+            // lindex
+            // 
+            this.lindex.AutoSize = true;
+            this.lindex.Location = new System.Drawing.Point(117, 39);
+            this.lindex.Name = "lindex";
+            this.lindex.Size = new System.Drawing.Size(0, 13);
+            this.lindex.TabIndex = 18;
+            // 
+            // cbIg
+            // 
+            this.cbIg.AutoSize = true;
+            this.cbIg.Location = new System.Drawing.Point(159, 37);
+            this.cbIg.Name = "cbIg";
+            this.cbIg.Size = new System.Drawing.Size(118, 17);
+            this.cbIg.TabIndex = 19;
+            this.cbIg.Text = "Ignore this question";
+            this.cbIg.UseVisualStyleBackColor = true;
+            // 
+            // cbAnimDelay
+            // 
+            this.cbAnimDelay.AutoSize = true;
+            this.cbAnimDelay.Checked = true;
+            this.cbAnimDelay.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbAnimDelay.Location = new System.Drawing.Point(418, 155);
+            this.cbAnimDelay.Name = "cbAnimDelay";
+            this.cbAnimDelay.Size = new System.Drawing.Size(135, 17);
+            this.cbAnimDelay.TabIndex = 20;
+            this.cbAnimDelay.Text = "Enable delay animation";
+            this.cbAnimDelay.UseVisualStyleBackColor = true;
+            // 
+            // cbEnbCol
+            // 
+            this.cbEnbCol.AutoSize = true;
+            this.cbEnbCol.Checked = true;
+            this.cbEnbCol.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbEnbCol.Location = new System.Drawing.Point(418, 179);
+            this.cbEnbCol.Name = "cbEnbCol";
+            this.cbEnbCol.Size = new System.Drawing.Size(91, 17);
+            this.cbEnbCol.TabIndex = 21;
+            this.cbEnbCol.Text = "Enable Colors";
+            this.cbEnbCol.UseVisualStyleBackColor = true;
+            this.cbEnbCol.CheckedChanged += new System.EventHandler(this.cbEnbCol_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(439, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Correct Color:";
+            // 
+            // tbCorr
+            // 
+            this.tbCorr.Location = new System.Drawing.Point(516, 202);
+            this.tbCorr.Name = "tbCorr";
+            this.tbCorr.Size = new System.Drawing.Size(70, 20);
+            this.tbCorr.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(439, 231);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Wrong Color:";
+            // 
+            // tbWrng
+            // 
+            this.tbWrng.Location = new System.Drawing.Point(516, 228);
+            this.tbWrng.Name = "tbWrng";
+            this.tbWrng.Size = new System.Drawing.Size(70, 20);
+            this.tbWrng.TabIndex = 25;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(600, 216);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(138, 13);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "(Enter HEX value for colors)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(207, 194);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(147, 13);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "<= mark the right answer here";
+            // 
+            // bDefCol
+            // 
+            this.bDefCol.Location = new System.Drawing.Point(442, 260);
+            this.bDefCol.Name = "bDefCol";
+            this.bDefCol.Size = new System.Drawing.Size(144, 23);
+            this.bDefCol.TabIndex = 28;
+            this.bDefCol.Text = "Restore Default Colors";
+            this.bDefCol.UseVisualStyleBackColor = true;
+            this.bDefCol.Click += new System.EventHandler(this.bDefCol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 470);
+            this.Controls.Add(this.bDefCol);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbWrng);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbCorr);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbEnbCol);
+            this.Controls.Add(this.cbAnimDelay);
+            this.Controls.Add(this.cbIg);
+            this.Controls.Add(this.lindex);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.bPrev);
+            this.Controls.Add(this.bNxt);
+            this.Controls.Add(this.bGen);
+            this.Controls.Add(this.rO4);
+            this.Controls.Add(this.rO3);
+            this.Controls.Add(this.rO2);
+            this.Controls.Add(this.rO1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbO4);
+            this.Controls.Add(this.tbO3);
+            this.Controls.Add(this.tbO2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbO1);
+            this.Controls.Add(this.tbQ);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Code Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbQ;
+        private System.Windows.Forms.TextBox tbO1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbO2;
+        private System.Windows.Forms.TextBox tbO3;
+        private System.Windows.Forms.TextBox tbO4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rO1;
+        private System.Windows.Forms.RadioButton rO2;
+        private System.Windows.Forms.RadioButton rO3;
+        private System.Windows.Forms.RadioButton rO4;
+        private System.Windows.Forms.Button bGen;
+        private System.Windows.Forms.Button bNxt;
+        private System.Windows.Forms.Button bPrev;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lindex;
+        private System.Windows.Forms.CheckBox cbIg;
+        private System.Windows.Forms.CheckBox cbAnimDelay;
+        private System.Windows.Forms.CheckBox cbEnbCol;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbCorr;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbWrng;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button bDefCol;
+    }
+}
+
---
+++ CodeGen/CodeGen/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CodeGen
+{
+    public partial class Form1 : Form
+    {
+        bool[] isIgnored = new bool[100];
+        private int index = 1, topIndex = 1;
+        int[] rytOps = new int[100];
+        string[] que = new string[100];
+        string[][] ops = new string[100][];
+        static string colorCor = "#090", colorWrng = "#D22";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            lindex.Text = index.ToString();
+            ops[index] = new string[4];
+            bPrev.Enabled = false;
+            restoreDefCol();
+        }
+        private void cbEnbCol_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbEnbCol.Checked)
+            {
+                tbCorr.Enabled = true;
+                tbWrng.Enabled = true;
+            }
+            else
+            {
+                tbCorr.Enabled = false;
+                tbWrng.Enabled = false;
+            }
+        }
+
+        void restoreDefCol() { tbCorr.Text = colorCor; tbWrng.Text = colorWrng; }
+        int checkRadio()
+        {
+            if (rO1.Checked)
+                return 1;
+            else if (rO2.Checked)
+                return 2;
+            else if (rO3.Checked)
+                return 3;
+            else if (rO4.Checked)
+                return 4;
+            else return 0;
+        }
+        void update()
+        {
+            que[index] = tbQ.Text;
+            ops[index][0] = tbO1.Text;
+            ops[index][1] = tbO2.Text;
+            ops[index][2] = tbO3.Text;
+            ops[index][3] = tbO4.Text;
+            rytOps[index] = checkRadio();
+            isIgnored[index] = cbIg.Checked;
+        }
+        void load()
+        {
+            tbQ.Text = que[index];
+            tbO1.Text = ops[index][0];
+            tbO2.Text = ops[index][1];
+            tbO3.Text = ops[index][2];
+            tbO4.Text = ops[index][3];
+            switch (rytOps[index])
+            {
+                case 1: rO1.Checked = true; break;
+                case 2: rO2.Checked = true; break;
+                case 3: rO3.Checked = true; break;
+                case 4: rO4.Checked = true; break;
+            }
+            cbIg.Checked = isIgnored[index];
+        }
+        void clear()
+        {
+            tbQ.Text = "";
+            tbO1.Text = "";
+            tbO2.Text = "";
+            tbO3.Text = "";
+            tbO4.Text = "";
+            rO1.Checked = false;
+            rO2.Checked = false;
+            rO3.Checked = false;
+            rO4.Checked = false;
+            cbIg.Checked = false;
+        }
+        bool askIgnoreMsg()
+        {
+            DialogResult result = MessageBox.Show("Please mark the correct answer\nOr click OK to ignore this question", "Fill required fields", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                cbIg.Checked = true;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void bPrev_Click(object sender, EventArgs e)
+        {
+            if (!cbIg.Checked && checkRadio() == 0)
+                if (!askIgnoreMsg())
+                    return;
+            
+            update();
+            clear();
+            index--;
+            lindex.Text = index.ToString();
+            load();
+            if (bPrev.Enabled == false) bPrev.Enabled = true;
+            if (index == 1) bPrev.Enabled = false;
+        }
+        private void bNxt_Click(object sender, EventArgs e)
+        {
+            if (!cbIg.Checked && checkRadio() == 0)
+                if (!askIgnoreMsg())
+                    return;
+
+            update();
+            clear();
+            index++;
+            lindex.Text = index.ToString();
+            if (index > topIndex)
+            {
+                topIndex++;
+                ops[index] = new string[4];
+            }
+            else load();
+            if (bPrev.Enabled == false) bPrev.Enabled = true;
+            if (index == 100) bNxt.Enabled = false;
+        }
+
+
+        private void bGen_Click(object sender, EventArgs e)
+        {
+            if (!cbIg.Checked && checkRadio() == 0)
+                if (!askIgnoreMsg())
+                    return;
+
+            update();
+
+            string code = "<!-- CodeGen aplha build -->\n\n";
+
+            for (int i = 1; i <= topIndex; i++)
+                if (!isIgnored[i])
+                    code += gen(i.ToString(), que[i], ops[i], rytOps[i]);
+
+            string[] script = { "<script>\n\tfunction corr(objL) { $(objL).text('Correct'); ",
+            "}\n\tfunction wrong(objL, rytAns){ $(objL).text('The answer is: ' + rytAns); ",
+            "}\n</script>\n\n" };
+
+            code += script[0];
+
+            if (cbEnbCol.Checked)
+                code += "$(objL).css('color', '" + tbCorr.Text + "'); ";
+            if (cbAnimDelay.Checked)
+                code += "$(objL).hide(); $(objL).fadeIn(170); ";
+
+            code += script[1];
+
+            if (cbEnbCol.Checked)
+                code += "$(objL).css('color', '" + tbWrng.Text + "'); ";
+            if(cbAnimDelay.Checked)
+                code += "$(objL).hide(); $(objL).fadeIn(500); ";
+
+            code += script[2];
+
+            Clipboard.SetText(code);
+            MessageBox.Show("HTML code has been generated and copied to clipboard, press Ctrl + V to paste it anywhere", "Code copied to clipboard", MessageBoxButtons.OK);
+        }
+
+
+        private string gen(string x, string q, string[] ops, int rytOpsNum)
+        {
+
+            string buff = q + "<br/>\n";
+
+            for (int i = 1; i <= 4; i++)
+            {
+                buff += "<input type='radio' name='" + x + "' onclick=";
+
+                if (i == rytOpsNum)
+                    buff += "\"corr('#res" + x + "');\"/>";
+                else
+                    buff += "\"wrong('#res" + x + "', '" + ops[rytOpsNum - 1] + "');\"/>";
+
+                buff += ops[i - 1] + "\n";
+            }
+
+            return buff += "</br><label id='res" + x + "'></label><br/><br/>\n\n";
+        }
+
+        private void bDefCol_Click(object sender, EventArgs e)
+        {
+            restoreDefCol();
+        }
+    }
+}
